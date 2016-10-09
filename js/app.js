41a71a5647deccd450a46241e46d7c62b31329ba
Mikka <folke@wp.pl>
2016-10-09 14:26:08 +0200

    initial react.js project

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+var Package = React.createClass({
+	render: function() {
+		return (
+			<div className="package">
+				<h3>Package</h3>
+			</div>
+		);
+	}
+});
+
+ReactDOM.render(<Package />, document.getElementById('content'));
